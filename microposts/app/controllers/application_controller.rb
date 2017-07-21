385991f27392ab0548c55c56878afdb5dd40af06
TASUKE <tasuke_0821@yahoo.co.jp>
2017-07-21 08:30:24 +0000

    follow, unfollow

--- microposts/app/controllers/application_controller.rb
+++ microposts/app/controllers/application_controller.rb
@@ -12,6 +12,9 @@ class ApplicationController < ActionController::Base
   end
   
   def counts(user)
-    @count_microposts =user.microposts.count
+    @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
+  
 end
